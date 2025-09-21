45e41aacf8de5beebc75119dde0ce09b534baf1a
Jackson Barnes <Jacksonbarnes246@gmail.com>
2025-09-20 19:56:44 -0600

    completed program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int gradepercentage = int.Parse(grade);
+
+        string lettergrade = "";
+
+        
+        if (gradepercentage >= 90)
+        {
+            lettergrade = "A";
+        }
+        else if (gradepercentage >= 80)
+        {
+            lettergrade = "B";
+        }
+        else if (gradepercentage >= 70)
+        {
+            lettergrade = "C";
+        }
+        else if (gradepercentage >= 60)
+        {
+            lettergrade = "D";
+        }
+        else
+        {
+            lettergrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {lettergrade}");
+        
+        if (gradepercentage >= 70)
+        {
+            Console.WriteLine("You passed your class great job!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't get it this time, good luck next time!");
+        }
+
     }
 }
