a241d975d12028873b4aebcf2190de82c951844e
Jackson Barnes <Jacksonbarnes246@gmail.com>
2025-09-25 12:28:22 -0600

    finished program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Diagnostics;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number: ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        Console.Write("What is your guess: ");
+        int guess = int.Parse(Console.ReadLine());
+
+        while (guess != magicNumber)
+        {
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            Console.WriteLine("Try again: ");
+            guess = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Correct! You guessed it!");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
-    }
+        List<string> fruits = new List<string>() { "Apple", "Banana", "Cherry" };
+        foreach (string fruit in fruits)
+
+            Console.WriteLine(fruit);
+
+        Console.WriteLine("Make a choice (y or n): ");
+        string value = Console.ReadLine().ToUpper();
+        while (value != "Y" || value != "N")
+
+            Console.Write("Choose y or n: ");
+        value = Console.ReadLine().ToUpper();
+
+
+        static int Adder(int a, int b);
+
+        {
+            return 0;
+        }
 }
+
+}
+
+
+
+
+
+
+
