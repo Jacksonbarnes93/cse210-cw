1bd6af4b349d2351ee1777b3cc249f50155555fb
Jackson Barnes <Jacksonbarnes246@gmail.com>
2025-09-25 19:08:42 -0600

    finished program

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+        var numbers = new List<int>();
+        while (true)
+        {
+            Console.Write("Enter a number (tpe 0 to stop): ");
+            string input = Console.ReadLine();
+
+            if (!int.TryParse(input, out int value))
+            {
+                Console.WriteLine("That wasn't a valid integer. please try again.");
+                continue;
+            }
+
+            if (value == 0)
+                break; 
+            numbers.Add(value);
+        }
+        if (numbers.Count > 0)
+        {
+            int sum = 0;
+            foreach (int n in numbers)
+                sum += n;
+
+            double average = (double)sum / numbers.Count;
+
+            int max = numbers[0];
+            foreach (int n in numbers)
+                if (n > max)
+                    max = n;
+
+            Console.WriteLine($"\nResults:");
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Average: {average}");
+            Console.WriteLine($"Max: {max}");
+        }
+        else
+        {
+            Console.WriteLine("No numbers were entered.");
+        }
     }
 }
