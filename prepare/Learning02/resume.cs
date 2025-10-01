51b441cce1d4fd95d58be0b3faf801ad51c4fcb0
Jacksonbarnes93 <63030461+Jacksonbarnes93@users.noreply.github.com>
2025-10-01 12:55:38 -0600

    Update resume.cs

--- prepare/Learning02/resume.cs
+++ prepare/Learning02/resume.cs
@@ -7,12 +7,13 @@ public class resume
     public List<Job> _jobs = new List<Job>();
     public void Display()
     {
-        Console.WriteLine($"Name: {_name}");
-        Console.WriteLine("Jobs:");
-        foreach (Job job in _jobs)
-        {
-            job.Display();
+    Console.WriteLine($"Name: {_name}");
+    Console.WriteLine("Jobs:");
+    foreach (Job job in _jobs)
+    {
+        job.Display();
         }
     }
 }
+
 
